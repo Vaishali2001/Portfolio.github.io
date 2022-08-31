90bcc28cdc2781d76b8dfa6a5178b8ac3725b1dd
Vaishali2001 <69723180+Vaishali2001@users.noreply.github.com>
2022-08-31 22:34:00 +0530

    My portfolio website.

---
+++ images/form.php
@@ -0,0 +1,35 @@
+<?php
+$server_name="localhost";
+$username="root";
+$password="";
+$database_name="database123";
+
+$conn=mysqli_connect($server_name,$username,$password,$database_name);
+//now check the connection
+if(!$conn)
+{
+	die("Connection Failed:" . mysqli_connect_error());
+
+}
+
+if(isset($_POST['save']))
+{	
+	 $name = $_POST['name'];
+	 $email = $_POST['email'];
+	 $phone = $_POST['phone'];
+	 $message = $_POST['message'];
+
+	 $sql_query = "INSERT INTO contact form details (Name, Mail ID, Phone_no, Message)
+	 VALUES ('$name','$email','$phone','$message')";
+
+	 if (mysqli_query($conn, $sql_query)) 
+	 {
+		echo "New Details Entry inserted successfully !";
+	 } 
+	 else
+     {
+		echo "Error: " . $sql . "" . mysqli_error($conn);
+	 }
+	 mysqli_close($conn);
+}
+?>
